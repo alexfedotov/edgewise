6f0e934ea6336085ee385a859b4d351a17e95b8c
Alex Fedotov <soyaxhoya@gmail.com>
2025-11-29 20:39:18 +0100

    Conduct minor improvements; add dijkstra unit-tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use rand::{Rng, rngs::ThreadRng};
 use std::collections::VecDeque;
 use std::fmt;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Weighted(u32);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Unweighted(());
+
 /// A graph represented as a vector of vectors, which
 /// models an adjacency list.
 ///
@@ -136,7 +141,7 @@ impl<W: InsertEdge> Graph<W> {
     }
 }
 
-impl Graph<u32> {
+impl Graph<Weighted> {
     pub fn dijkstra(&self, starting_node: u32) -> Option<Vec<Option<u32>>> {
         if (starting_node as usize) >= self.graph.len() {
             // Starting node does not exist, so we return a None
@@ -153,7 +158,8 @@ impl Graph<u32> {
             {
                 if let Some(neighbors) = self.graph.get(current_node as usize) {
                     for &(neighbor_node, neighbor_weight) in neighbors {
-                        if let Some(new_distance) = current_distance.checked_add(neighbor_weight) {
+                        if let Some(new_distance) = current_distance.checked_add(neighbor_weight.0)
+                        {
                             if let Some(neighbor_distance) = nodes_distance[neighbor_node as usize]
                             {
                                 if new_distance < neighbor_distance {
@@ -181,42 +187,54 @@ trait InsertEdge: Sized {
     fn insert_edge(g: &mut Graph<Self>, rng: &mut ThreadRng, i: u32, j: u32, is_directed: bool);
 }
 
-impl InsertEdge for () {
-    fn insert_edge(g: &mut Graph<()>, _rng: &mut ThreadRng, i: u32, j: u32, is_directed: bool) {
+impl InsertEdge for Unweighted {
+    fn insert_edge(
+        g: &mut Graph<Unweighted>,
+        _rng: &mut ThreadRng,
+        i: u32,
+        j: u32,
+        is_directed: bool,
+    ) {
         let u = g
             .graph
             .get_mut(i as usize)
             .expect(&format!("Node {i} is out of bounds"));
-        u.push((j, ()));
+        u.push((j, Unweighted(())));
         if !is_directed {
             let v = g
                 .graph
                 .get_mut(j as usize)
                 .expect(&format!("Node {j} is out of bounds"));
-            v.push((i, ()));
+            v.push((i, Unweighted(())));
         }
     }
 }
 
-impl InsertEdge for u32 {
-    fn insert_edge(g: &mut Graph<u32>, rng: &mut ThreadRng, i: u32, j: u32, is_directed: bool) {
+impl InsertEdge for Weighted {
+    fn insert_edge(
+        g: &mut Graph<Weighted>,
+        rng: &mut ThreadRng,
+        i: u32,
+        j: u32,
+        is_directed: bool,
+    ) {
         let w: u32 = rng.random_range(1..=10);
         let u = g
             .graph
             .get_mut(i as usize)
             .expect(&format!("Node {i} is out of bounds"));
-        u.push((j, w));
+        u.push((j, Weighted(w)));
         if !is_directed {
             let v = g
                 .graph
                 .get_mut(j as usize)
                 .expect(&format!("Node {j} is out of bounds"));
-            v.push((i, w));
+            v.push((i, Weighted(w)));
         }
     }
 }
 
-impl fmt::Display for Graph<()> {
+impl fmt::Display for Graph<Unweighted> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let i = self.edges();
         for (x, y, _) in i {
@@ -226,11 +244,11 @@ impl fmt::Display for Graph<()> {
     }
 }
 
-impl fmt::Display for Graph<u32> {
+impl fmt::Display for Graph<Weighted> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let i = self.edges();
         for (x, y, w) in i {
-            writeln!(f, "{x}-({w})->{y}")?;
+            writeln!(f, "{x}-({})->{y}", w.0)?;
         }
         Ok(())
     }
@@ -241,34 +259,61 @@ mod tests {
     use super::*;
     use once_cell::sync::Lazy;
 
-    static TEST_GRAPH: Lazy<Graph<()>> = Lazy::new(|| {
+    static TEST_GRAPH_UNWEIGHTED: Lazy<Graph<Unweighted>> = Lazy::new(|| {
         Graph::new(vec![
-            vec![(1, ()), (2, ()), (5, ())],
-            vec![(0, ()), (5, ())],
-            vec![(0, ())],
-            vec![(4, ())],
-            vec![(3, ())],
-            vec![(0, ())],
+            vec![
+                (1, Unweighted(())),
+                (2, Unweighted(())),
+                (5, Unweighted(())),
+            ], // 0
+            vec![(0, Unweighted(())), (5, Unweighted(()))], // 1
+            vec![(0, Unweighted(()))],                      // 2
+            vec![(4, Unweighted(()))],                      // 3
+            vec![(3, Unweighted(()))],                      // 4
+            vec![(0, Unweighted(()))],                      // 5
+        ])
+    });
+
+    static TEST_GRAPH_WEIGHTED: Lazy<Graph<Weighted>> = Lazy::new(|| {
+        Graph::new(vec![
+            vec![(1, Weighted(4)), (2, Weighted(1))], // 0
+            vec![(3, Weighted(1)), (4, Weighted(7))], // 1
+            vec![(1, Weighted(2)), (3, Weighted(5)), (5, Weighted(8))], // 2
+            vec![(6, Weighted(3))],                   // 3
+            vec![(6, Weighted(2)), (7, Weighted(3))], // 4
+            vec![(4, Weighted(2)), (8, Weighted(6))], // 5
+            vec![(9, Weighted(4))],                   // 6
+            vec![(6, Weighted(3)), (9, Weighted(2))], // 7
+            vec![(7, Weighted(1)), (9, Weighted(8))], // 8
+            vec![(5, Weighted(1))],                   // 9
+            // island
+            vec![(11, Weighted(3))],  // 10
+            vec![(12, Weighted(4))],  // 11
+            vec![(13, Weighted(2))],  // 12
+            vec![(10, Weighted(10))], // 13
+            vec![(12, Weighted(1))],  // 14
         ])
     });
 
     #[test]
     fn random_gen_weights_gr_zero() {
-        let g: Graph<u32> = Graph::random_graph(10, 0.5, true);
+        let g: Graph<Weighted> = Graph::random_graph(10, 0.5, true);
         for (_, _, w) in g.edges() {
-            assert!(*w > 0)
+            assert!(*w > Weighted(0))
         }
     }
 
     #[test]
     fn random_gen_undirected_weigh_simmetry() {
-        let g: Graph<u32> = Graph::random_graph(10, 0.5, false);
+        let g: Graph<Weighted> = Graph::random_graph(10, 0.5, false);
         let edges: Vec<_> = g.edges().collect();
         for &(u, v, w) in &edges {
             if let Some(&(_, _, w1)) = edges.iter().find(|&&(u1, v1, _)| u1 == v && v1 == u) {
                 assert!(
                     w == w1,
-                    "Symmetric edges {u}-({w})->{v} and {v}-({w1})->{u} have non-symmetric weights."
+                    "Symmetric edges {u}-({})->{v} and {v}-({})->{u} have non-symmetric weights.",
+                    w.0,
+                    w1.0
                 )
             } else {
                 panic!(
@@ -280,43 +325,101 @@ mod tests {
 
     #[test]
     fn basic_bfs_test() {
-        let mut bfs_result_start_from_0 = TEST_GRAPH
+        let mut bfs_result_start_from_0 = TEST_GRAPH_UNWEIGHTED
             .bfs(0)
             .expect("bfs(0) returned None unexpectedly");
         bfs_result_start_from_0.sort();
         let bfs_expected_result_start_from_0 = vec![0, 1, 2, 5];
         assert_eq!(bfs_result_start_from_0, bfs_expected_result_start_from_0);
-        let mut bfs_result_start_from_4 = TEST_GRAPH
+        let mut bfs_result_start_from_4 = TEST_GRAPH_UNWEIGHTED
             .bfs(4)
             .expect("bfs(4) returned None unexpectedly");
         bfs_result_start_from_4.sort();
         let bfs_expected_result_start_from_4 = vec![3, 4];
         assert_eq!(bfs_result_start_from_4, bfs_expected_result_start_from_4);
+        let bfs_result_start_from_6 = TEST_GRAPH_UNWEIGHTED.bfs(6);
+        assert_eq!(bfs_result_start_from_6, None);
     }
 
     #[test]
     fn basic_dfs_test() {
-        let mut dfs_result_start_from_0 = TEST_GRAPH
+        let mut dfs_result_start_from_0 = TEST_GRAPH_UNWEIGHTED
             .dfs(0)
             .expect("dfs(0) returned None unexpectedly");
         dfs_result_start_from_0.sort();
         let dfs_expected_result_start_from_0 = vec![0, 1, 2, 5];
         assert_eq!(dfs_result_start_from_0, dfs_expected_result_start_from_0);
-        let mut dfs_result_start_from_4 = TEST_GRAPH
+        let mut dfs_result_start_from_4 = TEST_GRAPH_UNWEIGHTED
             .dfs(4)
             .expect("dfs(4) returned None unexpectedly");
         dfs_result_start_from_4.sort();
         let dfs_expected_result_start_from_4 = vec![3, 4];
         assert_eq!(dfs_result_start_from_4, dfs_expected_result_start_from_4);
+        let dfs_result_start_from_6 = TEST_GRAPH_UNWEIGHTED.dfs(6);
+        assert_eq!(dfs_result_start_from_6, None);
     }
 
     #[test]
     fn bfs_dfs_equal_test() {
-        let g: Graph<u32> = Graph::random_graph(20, 0.5, true);
+        let g: Graph<Weighted> = Graph::random_graph(20, 0.5, true);
         let mut bfs_result = g.bfs(0).expect("bfs(0) returned None unexpectedly");
         bfs_result.sort();
         let mut dfs_result = g.dfs(0).expect("bfs(0) returned None unexpectedly");
         dfs_result.sort();
         assert_eq!(bfs_result, dfs_result);
+    }
+
+    #[test]
+    fn basic_dijkstra_test() {
+        let dijkstra_result_start_from_0 = TEST_GRAPH_WEIGHTED
+            .dijkstra(0)
+            .expect("dijkstra(0) returned None unexpectedly");
+        let dijkstra_expected_result_start_from_0 = [
+            Some(0),  // 0
+            Some(3),  // 1
+            Some(1),  // 2
+            Some(4),  // 3
+            Some(10), // 4
+            Some(9),  // 5
+            Some(7),  // 6
+            Some(13), // 7
+            Some(15), // 8
+            Some(11), // 9
+            None,     // 10
+            None,     // 11
+            None,     // 12
+            None,     // 13
+            None,     // 14
+        ];
+        assert_eq!(
+            dijkstra_result_start_from_0,
+            dijkstra_expected_result_start_from_0
+        );
+        let dijkstra_result_start_from_10 = TEST_GRAPH_WEIGHTED
+            .dijkstra(10)
+            .expect("dijkstra(10) returned None unexpectedly");
+        let dijkstra_expected_result_start_from_10 = [
+            None,    // 0
+            None,    // 1
+            None,    // 2
+            None,    // 3
+            None,    // 4
+            None,    // 5
+            None,    // 6
+            None,    // 7
+            None,    // 8
+            None,    // 9
+            Some(0), // 10
+            Some(3), // 11
+            Some(7), // 12
+            Some(9), // 13
+            None,    // 14
+        ];
+        assert_eq!(
+            dijkstra_result_start_from_10,
+            dijkstra_expected_result_start_from_10
+        );
+        let dijkstra_result_start_from_15 = TEST_GRAPH_WEIGHTED.dijkstra(15);
+        assert_eq!(dijkstra_result_start_from_15, None);
     }
 }
